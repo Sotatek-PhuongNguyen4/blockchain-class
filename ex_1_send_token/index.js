53af962beca7877c8af56e8daf6b5cbcdb85b4da
Sotatek-PhuongNguyen4 <phuong.nguyen4@sotatek.com>
2022-04-07 16:37:55 +0700

    fix private key parameter

--- ex_1_send_token/index.js
+++ ex_1_send_token/index.js
@@ -2,61 +2,50 @@ require('dotenv').config()
 const ethers = require('ethers')
 
 const NETWORK = process.env.NETWORK
+const SENDER_PRIVATE_KEY = process.env.SENDER_PRIVATE_KEY
 
-// provider is a object which help to connect to the Ethereum Network. 
+// provider is a object which help to connect to the Ethereum Network.
 // It provides read-only access to the Blockchain and its status.
 const provider = new ethers.providers.InfuraProvider(NETWORK)
 
-const sendTransaction = async (senderAddress, receiverAddress, amountToken, privateKeyOfSender) => {
-    // create signer object from it's private key to connect to network
-    const wallet = new ethers.Wallet(privateKeyOfSender)
+const sendTransaction = async (senderAddress, receiverAddress, amountToken) => {
+	// create signer object from it's private key to connect to network
+	const wallet = new ethers.Wallet(SENDER_PRIVATE_KEY)
 
+	// inject provider to signer to sign message and execute txn
+	const signer = wallet.connect(provider)
 
-    // inject provider to signer to sign message and execute txn
-    const signer = wallet.connect(provider)
-    
-    
-    // get getPrice for tx - BigNumber
-    const gasPriceBigNumber =  await provider.getGasPrice()
-   
-    const gasPriceHex = gasPriceBigNumber.toHexString()
+	// get getPrice for tx - BigNumber
+	const gasPriceBigNumber = await provider.getGasPrice()
 
-    // define gas limit
-    const  gasLimit = "0x100000"
+	const gasPriceHex = gasPriceBigNumber.toHexString()
 
-    // define input of sendTransaction
-    const tx = {
-        from: senderAddress,
-        to: receiverAddress,
-        value: ethers.utils.parseEther(amountToken),
-        nonce: provider.getTransactionCount(
-            senderAddress,
-          "latest"
-        ),
-        gasLimit: ethers.utils.hexlify(gasLimit),
-        gasPrice: gasPriceHex,
-      }
+	// define gas limit
+	const gasLimit = "0x100000"
 
-      try {
-        const transactionResult = await signer.sendTransaction(tx)
-        if (transactionResult) {
-            console.log("Send token successfully!!!")
-            console.log(transactionResult)
-        }
-      } catch (error) {
-            console.log("Send token failed!!!")
-      }
+	// define input of sendTransaction
+	const tx = {
+		from: senderAddress,
+		to: receiverAddress,
+		value: ethers.utils.parseEther(amountToken),
+		nonce: provider.getTransactionCount(senderAddress, "latest"),
+		gasLimit: ethers.utils.hexlify(gasLimit),
+		gasPrice: gasPriceHex,
+	}
+
+	try {
+		const transactionResult = await signer.sendTransaction(tx)
+		if (transactionResult) {
+			console.log("Send token successfully!!!")
+			console.log(transactionResult)
+		}
+	} catch (error) {
+		console.log("Send token failed!!!")
+	}
 }
 
-
 const SENDER_ADDRESS = process.env.SENDER_ADDRESS
-const SENDER_PRIVATE_KEY = process.env.SENDER_PRIVATE_KEY
 const RECEIVER_ADDRESS = process.env.RECEIVER_ADDRESS
 const AMOUNT_ETH_TOKEN = process.env.AMOUNT_ETH_TOKEN
 
-sendTransaction(
-	SENDER_ADDRESS,
-	RECEIVER_ADDRESS,
-	AMOUNT_ETH_TOKEN,
-	SENDER_PRIVATE_KEY
-)
+sendTransaction(SENDER_ADDRESS, RECEIVER_ADDRESS, AMOUNT_ETH_TOKEN)
